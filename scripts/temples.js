6823e094a0a270fea8ebccf5910fff9ef83da7ca
Antoniozabala123 <lukinkeri051@gmail.com>
2026-05-19 13:24:49 -0400

    add finshing

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,6 +7,6 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 
-    // Cambiar el texto del botón
-    hamButton.textContent = navigation.classList.contains('open') ? '☰' : '☰';
+    
+    hamButton.textContent = navigation.classList.contains('open') ? 'X' : '☰';
 });
