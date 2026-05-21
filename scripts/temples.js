63c57b6513d83c609885e9346c19d3dda7434d32
Antoniozabala123 <lukinkeri051@gmail.com>
2026-05-21 16:14:20 -0400

    add  years

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,9 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('nav'); 
+
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
