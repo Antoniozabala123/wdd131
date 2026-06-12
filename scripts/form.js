cf9558d29e7fe3ae410ffaae330f95e1c66109a6
Antoniozabala123 <lukinkeri051@gmail.com>
2026-06-12 05:17:31 -0400

    add

--- scripts/form.js
+++ scripts/form.js
@@ -2,12 +2,33 @@ document.getElementById("currentYear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
 
-    const products = [
-        { id: "banana", name: "banana", averagerating: 4.5 },
-        { id: "apple", name: "apple", averagerating: 4.6 },
-        { id: "orange", name: "orange", averagerating: 4.7 },
-        { id: "lemon", name: "lemon", averagerating: 4.8 },
-    ];
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
 
 document.addEventListener("DOMContentLoaded", () => {
     const productSelect = document.getElementById("product");
