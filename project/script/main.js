4d9afa84a6a6ea207921aebd0fbc9fc179a3db95
Antoniozabala123 <lukinkeri051@gmail.com>
2026-06-17 03:35:20 -0400

    add

--- project/script/main.js
+++ project/script/main.js
@@ -40,27 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const contactForm = document.getElementById("contact-form");
-    const displayElement = document.getElementById("reviewDisplay");
-
-    // ESCENARIO A: Si el usuario está en la página del formulario
-    if (contactForm) {
-        contactForm.addEventListener("submit", () => {
-            // Solo sumamos cuando el usuario hace clic de verdad en "Enviar/Subscribe"
-            let reviewCount = parseInt(localStorage.getItem("reviewCounter")) || 0;
-            reviewCount += 1;
-            localStorage.setItem("reviewCounter", reviewCount);
-        });
-    }
-
-    // ESCENARIO B: Si el usuario ya está en la página de agradecimiento (thank.html)
-    if (displayElement) {
-        // Solo leemos el valor actual del localStorage, NO sumamos aquí
-        const currentCount = localStorage.getItem("reviewCounter") || 0;
-        displayElement.textContent = currentCount;
-    }
-});
 
 
 document.getElementById("currentYear").textContent = new Date().getFullYear();
